33009cebe1b9dbf17eec768d5631bb530c68f8ea
nakamura196 <na.kamura.1263@gmail.com>
2021-07-06 10:14:26 +0900

    minor update

--- src/100_download.sh
+++ src/100_download.sh
@@ -1,7 +1,7 @@
 python 001_download.py items --isInitOutputDir True
 python 001_download.py item_sets --isInitOutputDir True
-python 001_download.py media --isInitOutputDir True
 python 001_download.py properties --isInitOutputDir True
 python 001_download.py resource_templates --isInitOutputDir True
 python 001_download.py sites --isInitOutputDir True
 python 001_download.py users --isInitOutputDir True
+python 001_download.py media --isInitOutputDir True
